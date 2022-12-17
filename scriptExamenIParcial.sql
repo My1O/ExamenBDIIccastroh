b7e5d9f13bc2f5d437f892825a9377a565141a1a
Camilo Castro <castroh24@gmail.com>
2022-12-17 10:23:57 -0600

    trigger alert with configuration done

--- scriptExamenIParcial.sql
+++ scriptExamenIParcial.sql
@@ -76,4 +76,21 @@ EXEC msdb.dbo.sp_send_dbmail
      @subject = 'Probando configuracion database mail';
 GO
 
+CREATE TRIGGER t_EmailAlert
+ON Production.ProductCategory
+AFTER INSERT, UPDATE, DELETE 
+AS 
+   RAISERROR (50005, -- ID del mensaje.
+           10, -- Severity,
+           1, -- Estado,
+           N'abcde'); -- First argument supplies the string.
+-- The message text returned is: <<    abc>>.
+   EXEC msdb.dbo.sp_send_dbmail 
+                        @profile_name = 'Camilo Castro', 
+                        @recipients = 'williamjsg@gmail.com' , 
+                        @body = 'Hay cambios en la Base de Datos AdventureWorks2019', 
+                        @subject = 'Hubo cambios en la tabla de ProductCategory' 
+
+GO
+
 SELECT * FROM msdb.dbo.sysmail_event_log;
